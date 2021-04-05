b144cd6abaef1c88893e9ed66ae58e7003765105
ReloJeffrey <relojeffrey@gmail.com>
2021-04-05 21:53:41 +0800

    first version

---
+++ self-correct/res.py
@@ -0,0 +1,511 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from torch.autograd import Variable
+import torch.nn.init as init
+# import torch.utils.model_zoo as model_zoo
+
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+    'resnext50_32x4d': 'https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth',
+    'resnext101_32x8d': 'https://download.pytorch.org/models/resnext101_32x8d-8ba56ff5.pth',
+    'wide_resnet50_2': 'https://download.pytorch.org/models/wide_resnet50_2-95faca4d.pth',
+    'wide_resnet101_2': 'https://download.pytorch.org/models/wide_resnet101_2-32ee1156.pth',
+}
+
+
+def to_var(x, requires_grad=True):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x, requires_grad=requires_grad)
+
+
+class MetaModule(nn.Module):
+    # adopted from: Adrien Ecoffet https://github.com/AdrienLE
+    def params(self):
+        for name, param in self.named_params(self):
+            yield param
+
+    def named_leaves(self):
+        return []
+
+    def named_submodules(self):
+        return []
+
+    def named_params(self, curr_module=None, memo=None, prefix=''):
+        if memo is None:
+            memo = set()
+
+        if hasattr(curr_module, 'named_leaves'):
+            for name, p in curr_module.named_leaves():
+                if p is not None and p not in memo:
+                    memo.add(p)
+                    yield prefix + ('.' if prefix else '') + name, p
+        else:
+            for name, p in curr_module._parameters.items():
+                if p is not None and p not in memo:
+                    memo.add(p)
+                    yield prefix + ('.' if prefix else '') + name, p
+
+        for mname, module in curr_module.named_children():
+            submodule_prefix = prefix + ('.' if prefix else '') + mname
+            for name, p in self.named_params(module, memo, submodule_prefix):
+                yield name, p
+
+    def update_params(self, lr_inner, first_order=False, source_params=None, detach=False):
+        if source_params is not None:
+            for tgt, src in zip(self.named_params(self), source_params):
+                name_t, param_t = tgt
+                # name_s, param_s = src
+                # grad = param_s.grad
+                # name_s, param_s = src
+                grad = src
+                if first_order:
+                    grad = to_var(grad.detach().data)
+                tmp = param_t - lr_inner * grad
+                self.set_param(self, name_t, tmp)
+        else:
+
+            for name, param in self.named_params(self):
+                if not detach:
+                    grad = param.grad
+                    if first_order:
+                        grad = to_var(grad.detach().data)
+                    tmp = param - lr_inner * grad
+                    self.set_param(self, name, tmp)
+                else:
+                    param = param.detach_()  # https://blog.csdn.net/qq_39709535/article/details/81866686
+                    self.set_param(self, name, param)
+
+    def set_param(self, curr_mod, name, param):
+        if '.' in name:
+            n = name.split('.')
+            module_name = n[0]
+            rest = '.'.join(n[1:])
+            for name, mod in curr_mod.named_children():
+                if module_name == name:
+                    self.set_param(mod, rest, param)
+                    break
+        else:
+            setattr(curr_mod, name, param)
+
+    def detach_params(self):
+        for name, param in self.named_params(self):
+            self.set_param(self, name, param.detach())
+
+    def copy(self, other, same_var=False):
+        for name, param in other.named_params():
+            if not same_var:
+                param = to_var(param.data.clone(), requires_grad=True)
+            self.set_param(name, param)
+
+
+class MetaLinear(MetaModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        ignore = nn.Linear(*args, **kwargs)
+
+        self.register_buffer('weight', to_var(ignore.weight.data, requires_grad=True))
+        self.register_buffer('bias', to_var(ignore.bias.data, requires_grad=True))
+
+    def forward(self, x):
+        return F.linear(x, self.weight, self.bias)
+
+    def named_leaves(self):
+        return [('weight', self.weight), ('bias', self.bias)]
+
+
+class MetaConv2d(MetaModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        ignore = nn.Conv2d(*args, **kwargs)
+
+        self.in_channels = ignore.in_channels
+        self.out_channels = ignore.out_channels
+        self.stride = ignore.stride
+        self.padding = ignore.padding
+        self.dilation = ignore.dilation
+        self.groups = ignore.groups
+        self.kernel_size = ignore.kernel_size
+
+        self.register_buffer('weight', to_var(ignore.weight.data, requires_grad=True))
+
+        if ignore.bias is not None:
+            self.register_buffer('bias', to_var(ignore.bias.data, requires_grad=True))
+        else:
+            self.register_buffer('bias', None)
+
+    def forward(self, x):
+        return F.conv2d(x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+
+    def named_leaves(self):
+        return [('weight', self.weight), ('bias', self.bias)]
+
+
+class MetaConvTranspose2d(MetaModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        ignore = nn.ConvTranspose2d(*args, **kwargs)
+
+        self.stride = ignore.stride
+        self.padding = ignore.padding
+        self.dilation = ignore.dilation
+        self.groups = ignore.groups
+
+        self.register_buffer('weight', to_var(ignore.weight.data, requires_grad=True))
+
+        if ignore.bias is not None:
+            self.register_buffer('bias', to_var(ignore.bias.data, requires_grad=True))
+        else:
+            self.register_buffer('bias', None)
+
+    def forward(self, x, output_size=None):
+        output_padding = self._output_padding(x, output_size)
+        return F.conv_transpose2d(x, self.weight, self.bias, self.stride, self.padding,
+                                  output_padding, self.groups, self.dilation)
+
+    def named_leaves(self):
+        return [('weight', self.weight), ('bias', self.bias)]
+
+
+class MetaBatchNorm2d(MetaModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        ignore = nn.BatchNorm2d(*args, **kwargs)
+
+        self.num_features = ignore.num_features
+        self.eps = ignore.eps
+        self.momentum = ignore.momentum
+        self.affine = ignore.affine
+        self.track_running_stats = ignore.track_running_stats
+
+        if self.affine:
+            self.register_buffer('weight', to_var(ignore.weight.data, requires_grad=True))
+            self.register_buffer('bias', to_var(ignore.bias.data, requires_grad=True))
+
+        if self.track_running_stats:
+            self.register_buffer('running_mean', torch.zeros(self.num_features))
+            self.register_buffer('running_var', torch.ones(self.num_features))
+        else:
+            self.register_parameter('running_mean', None)
+            self.register_parameter('running_var', None)
+
+    def forward(self, x):
+        return F.batch_norm(x, self.running_mean, self.running_var, self.weight, self.bias,
+                            self.training or not self.track_running_stats, self.momentum, self.eps)
+
+    def named_leaves(self):
+        return [('weight', self.weight), ('bias', self.bias)]
+
+
+class LambdaLayer(MetaModule):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+
+
+class VNet(MetaModule):
+    def __init__(self, input, hidden1, output):
+        super(VNet, self).__init__()
+        self.linear1 = MetaLinear(input, hidden1)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.linear2 = MetaLinear(hidden1, output)
+        # self.linear3 = MetaLinear(hidden2, output)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu1(x)
+        # x = self.linear2(x)
+        # x = self.relu1(x)
+        out = self.linear2(x)
+        return F.sigmoid(out)
+
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return MetaConv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return MetaConv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(MetaModule):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = MetaBatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(MetaModule):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = MetaBatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(MetaModule):
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = MetaBatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = MetaConv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = MetaLinear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, MetaConv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        #x = torch.flatten(x, 1)
+        #print('size:', x.shape)
+        x = torch.reshape(x, (x.shape[0], -1))
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, x):
+        return self._forward_impl(x)
+
+
+# def _resnet(arch, block, layers, pretrained, progress, **kwargs):
+#     model = ResNet(block, layers, **kwargs)
+#     if pretrained:
+#         state_dict = torch.hub.load_state_dict_from_url(model_urls[arch], progress=progress)
+#         model.load_state_dict(state_dict)
+#     return model
+
+
+# def resnet18(pretrained=False, progress=True, **kwargs):
+#     return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], pretrained, progress,
+#                    **kwargs)
+
+
+
+# def resnet34(pretrained=False, progress=True, **kwargs):
+#     return _resnet('resnet34', BasicBlock, [3, 4, 6, 3], pretrained, progress,
+#                    **kwargs)
+
+
+
+# def resnet50(pretrained=False, progress=True, **kwargs):
+#     return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], pretrained, progress,
+#                    **kwargs)
+
+
+
+# def resnet101(pretrained=False, progress=True, **kwargs):
+#     return _resnet('resnet101', Bottleneck, [3, 4, 23, 3], pretrained, progress,
+#                    **kwargs)
+
+
+
+# def resnet152(pretrained=False, progress=True, **kwargs):
+#     return _resnet('resnet152', Bottleneck, [3, 8, 36, 3], pretrained, progress,
+#                    **kwargs)
+
+
+
+# def resnext50_32x4d(pretrained=False, progress=True, **kwargs):
+#     kwargs['groups'] = 32
+#     kwargs['width_per_group'] = 4
+#     return _resnet('resnext50_32x4d', Bottleneck, [3, 4, 6, 3],
+#                    pretrained, progress, **kwargs)
+
+
+
+# def resnext101_32x8d(pretrained=False, progress=True, **kwargs):
+#     kwargs['groups'] = 32
+#     kwargs['width_per_group'] = 8
+#     return _resnet('resnext101_32x8d', Bottleneck, [3, 4, 23, 3],
+#                    pretrained, progress, **kwargs)
+
+
+
+# def wide_resnet50_2(pretrained=False, progress=True, **kwargs):
+#     kwargs['width_per_group'] = 64 * 2
+#     return _resnet('wide_resnet50_2', Bottleneck, [3, 4, 6, 3],
+#                    pretrained, progress, **kwargs)
+
+
+
+# def wide_resnet101_2(pretrained=False, progress=True, **kwargs):
+#     kwargs['width_per_group'] = 64 * 2
+#     return _resnet('wide_resnet101_2', Bottleneck, [3, 4, 23, 3],
+#                    pretrained, progress, **kwargs)
+
+
+def ResNet34(num, pretrained=False):   
+    if pretrained:
+        model = ResNet(block=BasicBlock, layers=[3, 4, 6, 3])
+        state_dict = torch.utils.model_zoo.load_url(model_urls['resnet34'], progress=True)
+        model.load_state_dict(state_dict)
+        model.fc = MetaLinear(512, num)
+    else:
+        model = ResNet(block=BasicBlock, layers=[3, 4, 6, 3], num_classes=num)
+
+    
+    return model
+    
+
+
